c6847df2a4ea5835dbc5c99a692135994f579710
Matheus <matheuscostadosantos@hotmail.com>
2021-08-14 17:56:46 -0300

    estrutura formulario javascript

--- uc09/sa1/2-formulario/_javascript/scripts.js
+++ uc09/sa1/2-formulario/_javascript/scripts.js
@@ -1,4 +1,4 @@
-// Cria a função Teste
+// Cria a função teste()
 function teste() {
     console.log(dados()); // Chama a função dados() e apresenta no console.log - debug do navegador
 
@@ -11,6 +11,7 @@ function teste() {
     }
 }
 
+// Cria a função verifica_cadastro()
 function verifica_cadastro() {
 
     // Cria a variavel resultado e retorna ela sempre como verdadeiro
@@ -35,15 +36,70 @@ function verifica_cadastro() {
     }
 
     // Verifica mes
-    var mes = parseInt(document.getElementById("month").value)
+    var mes = parseInt(document.getElementById("month").value) // função parseInt converte o texto digitado pelo usuario no tipo inteiro
     if ((mes < 1) || (mes > 12) || (isNaN(mes))) {
         resultado = false;
     }
 
     // Verifica ano
-    var ano_atual = new Date().getFullYear();
+    var ano_atual = new Date().getFullYear(); // armazena a data atual na variavel "ano_atual"
     var ano = parseInt(document.getElementById("year").value);
     if ((ano < 1900) || (ano > ano_atual) || (isNaN(ano))) {
         resultado = false;
     }
+
+    // Verifica CPF
+    var expressao = /[0-9][0-9][0-9]\.[0-9][0-9][0-9]\.[0-9][0-9][0-9]\-[0-9][0-9]/; // Regex - formatação do campo CPF
+    var cpf = document.getElementById("cpf").value;
+    if (!(expressao.test(cpf))) { // (!) se nao for - expressao
+        resultado = false;
+    }
+    
+    // Verifica times
+    // var times = document.getElementsByTagName("option");
+
+    // Verifica esportes
+    var esportes = document.getElementsByTagName("sport");
+    var cont_esporte = 0;
+    for (var i = 0; i < esportes.length; ++i) {
+        if (esportes[i].checked) {
+            cont_esporte = cont_esporte + 1;
+        }
+    }
+    if ((cont_esporte == 0)) {
+        resultado = false;
+    }
+    return resultado; // retorno da função: verifica_cadastro()
+}
+
+// Cria a função dados()
+function dados() {
+    var cadastro = new Object();
+
+    cadastro.name = document.getElementById("name").value;
+
+    cadastro.nickname = document.getElementById("nickname").value;
+
+    cadastro.birth_date = document.getElementById("year").value;
+        document.getElementById("month").value + "-" +
+        document.getElementById("day").value;
+
+    cadastro.cpf = document.getElementById("cpf").value;
+
+    var times = document.getElementsByTagName("option");
+    for (var i = 0; i < times.length; ++i) {
+        if (times[i].selected) {
+            cadastro.team_id = times[i].id;
+        }
+    }
+
+    var esportes = document.getElementsByName("sport");
+    cadastro.sport = [];
+    for (var i = 0; i < esportes.length; ++i) {
+        if (esportes[i].checked) {
+            cadastro.sport.push(esportes[i].id);
+        }
+    }
+
+    return cadastro;
 }
