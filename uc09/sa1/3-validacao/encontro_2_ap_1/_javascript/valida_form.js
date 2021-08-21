3c8ef63e8043e9c33185c4f253ec2ba522866c1a
Matheus <matheuscostadosantos@hotmail.com>
2021-08-21 16:41:37 -0300

    regex - expressoes regulares

--- uc09/sa1/3-validacao/encontro_2_ap_1/_javascript/valida_form.js
+++ uc09/sa1/3-validacao/encontro_2_ap_1/_javascript/valida_form.js
@@ -1,6 +1,5 @@
 //função para totalizar valor de assinatura mensal
-function totaliza()
-{
+function totaliza() {
     total = 0.00;
     //plano mensal
     if(document.form.planos[0].checked)
@@ -29,12 +28,13 @@ function validarNome() {
       alert('Somente caracteres alfabéticos Inválido');
       document.form.nome.focus(); // Seta o foco no input com id "nome"
       return false;
+    } else {
+        return true;
     }
-    return true;
-  }
+}
 
 // Validação com REGEX - CPF
-function validar_cpf() {
+function validarCPF() {
     let value = document.getElementById("cpf").value;
     let re = /^(([0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2})|([0-9]{11}))$/;
     if (!re.test(value)) {
@@ -42,6 +42,51 @@ function validar_cpf() {
       alert('Conteúdo de CPF Inválido');
       document.form.cpf.focus();
       return false;
+    } else {
+        return true;
     }
-    return true;
-  }
+}
+
+// Validação com REGEX - E-mail
+function validarEmail() {
+    let emailValue = document.getElementById("email").value;
+    let emailRegex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
+    if (!emailRegex.test(emailValue)) {
+        alert('Email Inválido!');
+        document.form.email.focus();
+        return false;
+    } else {
+        return true;
+    }
+}
+
+// Validação com REGEX - Celular
+function validarCelular() {
+    let celularValue = document.getElementById("cel").value;
+    // Formato válido: (XX) XXXXX-XXXX | (ou) XXXXXXXXXXX
+    let celularRegex = /^((\([0-9]{2}\) [0-9]{5}\-[0-9]{4}) | ([0-9{11}]))$/;
+    if (!celularRegex.test(celularValue)) {
+        alert("Celular inválido!");
+        document.form.cel.focus();
+        return false;
+    } else {
+        return true;
+    }
+}
+
+// Validação com REGEX - Planos
+function validaPlanos() {
+    let planoValue = document.getElementsByName("planos");
+    let planoCkecked = false;
+    for (i = 0; i < planoValue.length; i++) {
+        if (planoValue[i].checked) {
+            planoCkecked = true;
+        }
+    }
+    if (!planoCkecked) {
+        alert("Selecione o Plano");
+        return false;
+    } else {
+        return true;
+    }
+}
