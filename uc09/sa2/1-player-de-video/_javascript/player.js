6edaf5d117cac1f6c9b9cddd8e9ee18f106144fd
Matheus Costa <matheuscostadosantos@hotmail.com>
2021-09-01 12:56:58 -0300

    funcao play-pause js

--- uc09/sa2/1-player-de-video/_javascript/player.js
+++ uc09/sa2/1-player-de-video/_javascript/player.js
@@ -1,5 +1,13 @@
 var meuVideo = document.getElementById("playerCustomizado");
 
+function playPause() {
+  if (meuVideo.paused) {
+    meuVideo.play();
+  } else {
+    meuVideo.pause();
+  }
+}
+
 function telaCheia() {
   meuVideo.requestFullscreen();
 }
