afd0d48a61edd74b4e5cd9b773d2c0668764a35e
Matheus Costa <matheuscostadosantos@hotmail.com>
2021-10-30 17:36:04 -0300

    View visualizarJogadores

--- uc10/sa1/atividade-3/8. db_copa_mundo view.sql
+++ uc10/sa1/atividade-3/8. db_copa_mundo view.sql
@@ -1,2 +1,8 @@
-create view clodovil as
-select 
+create view visualizarJogadores as
+select p.selecao, j.nome, j.distancia, j.saltos, j.giros, j.impactos
+from jogador j
+inner join pais p on j.pais_idpais = p.idpais
+where j.distancia is not null
+order by p.selecao, j.distancia asc;
+
+select * from visualizarJogadores;
