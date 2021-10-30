f1f397af240535c1e954b81b3199a7c3888dbc7b
Matheus Costa <matheuscostadosantos@hotmail.com>
2021-10-30 15:38:05 -0300

    Ajuste scripts

---
+++ uc10/sa1/atividade-3/5. db_copa_mundo selects.sql
@@ -0,0 +1,22 @@
+/* Mostra as colunas da tabela */
+desc pais;
+
+/* Mostra todos os dados da tabela */
+select * from pais;
+
+/* ---------------------------------------- */;
+
+/* Nome de todos os jogadores titulares, por seleção */
+select p.selecao, j.nome from jogador j
+inner join pais p on j.pais_idpais = p.idpais
+where j.situacao = "T"
+order by p.selecao, j.nome;
+
+/* Seleção, o nome do jogador e número de vezes que ele foi substituído */
+select p.selecao, j.nome, count(s.jogador_idjogador_sai) as substituicao
+from substituicao s
+inner join jogador j on s.jogador_idjogador_sai = j.idjogador
+inner join pais p on j.pais_idpais = p.idpais
+group by p.selecao, j.nome;
+
+select * from substituicao;
