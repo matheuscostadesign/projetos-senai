c928dc9b1e1b6782d5737271461d95ff660a8b3a
Matheus Costa <matheuscostadosantos@hotmail.com>
2022-02-14 13:15:28 -0300

    Mural vagas componente 09:56

--- uc12/sa2/angular/lh-angular/src/app/app.module.ts
+++ uc12/sa2/angular/lh-angular/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MuralVagasComponent } from './mural-vagas/mural-vagas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MuralVagasComponent
   ],
   imports: [
     BrowserModule
