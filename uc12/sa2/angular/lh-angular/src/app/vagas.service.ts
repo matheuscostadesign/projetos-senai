f3a86acbf921763495bedd67c17b362f591947c9
Matheus Costa <matheuscostadosantos@hotmail.com>
2022-02-16 13:40:52 -0300

    Finalizacao aula 07 e ajustes layout

--- uc12/sa2/angular/lh-angular/src/app/vagas.service.ts
+++ uc12/sa2/angular/lh-angular/src/app/vagas.service.ts
@@ -10,10 +10,23 @@ export class VagasService {
 
   private url = "http://localhost:3000/vagas";
 
-  constructor(private _httpClient: HttpClient) { }
+  constructor(private _httpClient: HttpClient) {  }
 
   getVagas(): Observable<Vaga[]>{
     return this._httpClient.get<Vaga[]>(this.url);
   }
 
+  cadastrarVaga(vaga: Vaga):Observable<Vaga[]>{
+    return this._httpClient.post<Vaga[]>(this.url,vaga);
+  }
+
+  atualizarVaga(id:any ,vaga: Vaga):Observable<Vaga[]>{
+    const urlAtualizar = `${this.url}/${id}`;
+    return this._httpClient.put<Vaga[]>(urlAtualizar,vaga);
+  }
+
+  removerVaga(id:any):Observable<Vaga[]>{
+    const urlDeletar = `${this.url}/${id}`;
+    return this._httpClient.delete<Vaga[]>(urlDeletar);
+  }
 }
